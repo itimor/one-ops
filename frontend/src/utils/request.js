d58ac42365536c4c62baace83d6a626afa44be75
itimor <itimor@126.com>
2020-08-07 06:49:48 +0800

    add

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -43,8 +43,8 @@ service.interceptors.response.use(
    * You can also judge the status by HTTP Status Code
    */
   response => {
-    console.log(response)
     const res = response.data
+    console.log(res)
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 20000) {
       Message({
