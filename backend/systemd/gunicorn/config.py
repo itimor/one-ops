a10ea0dce8cdf93b4de2a7f84284a3967808be04
itimor <itimor@126.com>
2020-08-10 10:30:26 +0800

    add

--- backend/systemd/gunicorn/config.py
+++ backend/systemd/gunicorn/config.py
@@ -10,7 +10,7 @@ import multiprocessing
 bind = "0.0.0.0:1688"  # 绑定的ip与端口
 backlog = 512  # 监听队列数量，64-2048
 chdir = '/data/projects/one-ops/backend'  # gunicorn要切换到的目的工作目录
-worker_class = 'gevent'  # 使用 gevent 模式，还可以使用 sync 模式，默认的是sync模式
+worker_class = 'sync'  # 使用 gevent 模式，还可以使用 sync 模式，默认的是sync模式
 workers = 6  # multiprocessing.cpu_count()    #进程数
 threads = 8  # multiprocessing.cpu_count()*4 #指定每个进程开启的线程数
 loglevel = 'debug'  # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
