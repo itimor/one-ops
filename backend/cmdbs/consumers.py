f313bc812fcc46f9bbed0f1f71b6823989f4facd
itimor <itimor@126.com>
2020-08-10 11:35:13 +0800

    add

--- backend/cmdbs/consumers.py
+++ backend/cmdbs/consumers.py
@@ -54,8 +54,8 @@ class TailfConsumer(WebsocketConsumer):
     def connect(self):
         self.filename = self.scope["url_route"]["kwargs"]["filename"]
         log_path = '/tmp/cmdb_log'
-        os.path.join(log_path, self.filename + '.log')
-        self.result = tailf.delay(log_path, self.channel_name)
+        log_full_path = os.path.join(log_path, self.filename + '.log')
+        self.result = tailf.delay(log_full_path, self.channel_name)
         print('connect:', self.channel_name, self.result.id)
         self.accept()
 
