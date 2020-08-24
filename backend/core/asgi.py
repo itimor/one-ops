b8d1ee25ef839082f4bbebc3d9c1bdb88d7c3e2c
itimor <itimor@126.com>
2020-08-24 14:09:06 +0800

    change asgi

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -8,4 +8,5 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
 application = get_default_application()
