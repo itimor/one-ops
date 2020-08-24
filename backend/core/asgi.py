cb9765363b063a1185c52bd931e6b070aab2be5f
itimor <itimor@126.com>
2020-08-24 14:07:48 +0800

    add

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -4,7 +4,8 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-application = get_asgi_application()
+application = get_default_application()
