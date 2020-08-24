086120800d920d6865e73232862fa6c4d9572bbb
itimor <itimor@126.com>
2020-08-24 14:14:39 +0800

    change asgi

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -3,10 +3,16 @@ ASGI entrypoint. Configures Django and then runs the application
 defined in the ASGI_APPLICATION setting.
 """
 
-import os
-import django
-from channels.routing import get_default_application
+# import os
+# import django
+# from channels.routing import get_default_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+# django.setup()
+# application = get_default_application()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-django.setup()
-application = get_default_application()
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+application = get_asgi_application()
